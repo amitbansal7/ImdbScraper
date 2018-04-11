9f68d9a6012cf2de8a34d12b8ae47b244ba97ec2
Amit Bansal <007amitbansal@gmail.com>
2018-04-12 00:42:31 +0530

    Added proxy support

--- build.sbt
+++ build.sbt
@@ -4,4 +4,8 @@ version := "0.1"
 
 scalaVersion := "2.12.5"
 
-libraryDependencies += "org.jsoup" % "jsoup" % "1.7.2"
+libraryDependencies += "org.jsoup" % "jsoup" % "1.11.2"
+
+libraryDependencies += "au.com.bytecode" % "opencsv" % "2.4"
+
+
--- src/main/scala/scraper/ReviewScraper.scala
+++ src/main/scala/scraper/ReviewScraper.scala
@@ -31,7 +31,38 @@ object ReviewScraper {
     }
   }
 
-  def getAllreviews(id: String, limit: Int): List[Review] = {
+  def getProxies(): Vector[(String, Int)] = {
+
+    def getAllTds(element: Element): Vector[String] = {
+      def helper(acc: Vector[String], elements: Elements, idx: Int): Vector[String] = {
+        if (idx >= elements.size()) acc
+        else helper(acc :+ elements.get(idx).text(), elements, idx + 1)
+      }
+
+      helper(Vector[String](), element.select("td"), 0)
+    }
+
+    def loopOver(elements: Elements, idx: Int, acc: Vector[(String, Int)]): Vector[(String, Int)] = {
+      if (idx <= 1) acc
+      else {
+        val allTds = getAllTds(elements.get(idx))
+        if (allTds.size == 8 && !allTds(4).equals("transparent") && !allTds(6).equals("no"))
+          loopOver(elements, idx - 1, (allTds(0), allTds(1).toInt) +: acc)
+        else loopOver(elements, idx - 1, acc)
+      }
+    }
+
+    def getJsoupDoc() =
+      Jsoup
+        .connect("https://www.us-proxy.org/")
+        .userAgent("Mozilla")
+        .get()
+
+    val elements = getJsoupDoc.attr("id", "proxylisttable").select("tr")
+    loopOver(elements, elements.size() - 1, Vector[(String, Int)]())
+  }
+
+  def getAllreviews(id: String, limit: Int, prox: (String, Int)): List[Review] = {
 
     def reviewToReviewModel(review: Element): List[Review] = {
 
@@ -62,20 +93,24 @@ object ReviewScraper {
       else scrapeReviewContainer(elements, idx + 1, acc ::: reviewToReviewModel(elements.get(idx)), count + 1, limit)
     }
 
+    def getJsoupDoc(url: String): Document =
+      Jsoup
+        .connect(url)
+        .userAgent("Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:25.0) Gecko/20100101 Firefox/25.0")
+        .referrer("http://www.google.com")
+        .proxy(prox._1, prox._2)
+        .get()
+
     def findWithKey(str: String, id: String, count: Int, limit: Int): List[Review] = {
       if (str.equals("") || count >= limit) Nil
       else {
-        val doc = Jsoup
-          .connect("http://www.imdb.com/title/" + id + "/reviews/_ajax?ref_=undefined&paginationKey=" + str)
-          .get()
-
+        val doc = getJsoupDoc("http://www.imdb.com/title/" + id + "/reviews/_ajax?ref_=undefined&paginationKey=" + str)
         val res = scrapeReviewContainer(doc.select("div[class=review-container]"), 0, Nil, count, limit)
         res ::: findWithKey(doc.select("div[class=load-more-data]").attr("data-key"), id, count + res.size, limit)
       }
     }
 
-    val baseUrl = "http://www.imdb.com/title/" + id + "/reviews/?sort=submissionDate&dir=desc"
-    val doc = Jsoup.connect(baseUrl).get()
+    val doc = getJsoupDoc("http://www.imdb.com/title/" + id + "/reviews/?sort=submissionDate&dir=desc")
     val res = scrapeReviewContainer(doc.select("div[class=review-container]"), 0, Nil, 0, limit)
     res ::: findWithKey(doc.select("div[class=load-more-data]").attr("data-key"), id, res.size, limit)
   }
@@ -84,14 +119,15 @@ object ReviewScraper {
   def main(args: Array[String]): Unit = {
 
     //Movie id
-    val id = "tt0111161";
+    val id = "tt0371746";
 
     //Maximum number of reviews
-    val limit = Int.MaxValue
+    val limit = 200
 
     println(s"Fetching reviews for $id")
     val start = System.currentTimeMillis()
-    val res = getAllreviews(id, limit)
+    val proxies = getProxies
+    val res = getAllreviews(id, limit, proxies(0))
     val end = System.currentTimeMillis()
 
     res.foreach {
